2ba2592f5f3d2ba9321684eecfc30f654530a805
xadicha <mahkamovahadija1810@gmail.com>
2024-09-12 11:16:50 +0500

    user-service qoshildi

---
+++ user-service/internal/pkg/register-service/user-service.go
@@ -0,0 +1,3 @@
+package registerservice
+
+func Register()
---
+++ user-service/internal/repository/repository.go
@@ -0,0 +1,13 @@
+package repository
+
+
+// type IUserRepository interface {
+// 	CreateUser(req *pb.CreateUserRequest) (*pb.CreateUserResponse, error)
+// 	LoginUser(req *pb.LoginUserRequest) (*pb.LoginUserResponse, error)
+// 	GetUserById(req *pb.GetUserByIdRequest) (*pb.GetUserByIdResponse, error)
+// 	ListOfUser(req *pb.ListOfUserRequest) (*pb.ListOfUserResponse, error)
+// 	UpdateUser(req *pb.UpdateUserRequest) (*pb.UpdateUserResponse, error)
+// 	DeleteUser(req *pb.DeleteUserRequest) (*pb.DeleteUserResponse, error)
+// }
+
+
